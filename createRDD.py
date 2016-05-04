3cb9da428f9f34db00ceb1875fa761afcce0ebd8
gaotian1 <gaotian1@lenovo.com>
2016-05-04 19:15:13 +0800

    RDD1

--- createRDD.py
+++ createRDD.py
@@ -2,4 +2,9 @@
 from pyspark import *
 from pyspark.streaming import StreamingContext
 
-#
+#创建RDD有两种方式：1.读取外部数据集;2.在驱动器程序中对一个集合进行并行化(传给SparkContext的parallelize()方法)
+#将程序中已有的集合，传给SparkContext的parallelize()方法
+
+sc = SparkContext("local", "parallizeApp")
+items = sc.parallize(["pandas", "I like pandas"])
+
--- textRDD.py
+++ textRDD.py
@@ -8,7 +8,7 @@ import time
 # python程序、shell本身都可以作为驱动器程序。
 
 sc = SparkContext("local", "firstApp")
-# sparkContext用于访问spark的对象
+# 驱动器程序通过sparkContext对象来访问spark
 distFile = sc.textFile("c:\Hadoop_data\lib\data.txt")
 # 对一个文本文档生成一个RDD
 # RDD支持两种类型的操作：转化操作（transformation）和行动操作（action），转化操作会生成新的RDD，行动操作会对RDD计算出一个结果
@@ -18,5 +18,12 @@ pythonFilter = distFile.filter(lambda lines:'s' in lines)
 # 转化操作：并行执行一个过滤操作，返回符合条件的一个RDD子集
 count = distFile.count()
 # 行动操作：统计RDD中元素的个数
-print count, longestLine, pythonFilter.reduce(lambda a, b: sorted([a, b])[0])
+# print count, longestLine, pythonFilter.reduce(lambda a, b: sorted([a, b])[0])
+enFilter = distFile.filter(lambda item: 'learning' in item)
+cnFilter = distFile.filter(lambda item: u'学习' in item)
+cnEnFilter = cnFilter.union(enFilter)
+# union是转化操作，将两个RDD组合起来
+for line in cnEnFilter.take(cnEnFilter.count()): # 打印出整个RDD所有的元素
+    print line
+
 
